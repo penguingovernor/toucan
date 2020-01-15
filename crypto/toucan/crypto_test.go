1673abde54e261587aa36a161f1fc80377f96197
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-01-14 17:55:08 -0800

    format tests

--- crypto/toucan/crypto_test.go
+++ crypto/toucan/crypto_test.go
@@ -10,44 +10,43 @@ func TestEncrypt(t *testing.T) {
 	msg := []byte("Hello World\n")
 
 	t.Run("successful encryption", func(t *testing.T) {
-	ct,err := Encrypt(msg,msg,msg)
-	if err != nil {
-		t.Fatalf("wanted err == nil, got err = %v", err)
-	}
-	want := "efrjHxns8qKwYtQJolQZdv6+HNRb1teO8xcxM1/3/LKJhsjvfYsXPOZLjXY="
-	wantCT, err := base64.StdEncoding.DecodeString(want)
-	if err != nil {
-		t.Fatalf("wanted err == nil, got err = %v", err)
-	}
-	if !bytes.Equal(wantCT,ct) {
-		t.Fatalf("want != got | %x != %x", ct, wantCT)
-	}
+		ct, err := Encrypt(msg, msg, msg)
+		if err != nil {
+			t.Fatalf("wanted err == nil, got err = %v", err)
+		}
+		want := "efrjHxns8qKwYtQJolQZdv6+HNRb1teO8xcxM1/3/LKJhsjvfYsXPOZLjXY="
+		wantCT, err := base64.StdEncoding.DecodeString(want)
+		if err != nil {
+			t.Fatalf("wanted err == nil, got err = %v", err)
+		}
+		if !bytes.Equal(wantCT, ct) {
+			t.Fatalf("want != got | %x != %x", ct, wantCT)
+		}
 	})
 
 }
 
 func TestDecrypt(t *testing.T) {
-	ct,err := base64.StdEncoding.DecodeString("efrjHxns8qKwYtQJolQZdv6+HNRb1teO8xcxM1/3/LKJhsjvfYsXPOZLjXY=")
+	ct, err := base64.StdEncoding.DecodeString("efrjHxns8qKwYtQJolQZdv6+HNRb1teO8xcxM1/3/LKJhsjvfYsXPOZLjXY=")
 	if err != nil {
 		t.Fatalf("failed to decode bin string")
 	}
 	msg := []byte("Hello World\n")
 
-
 	t.Run("successful decryption", func(t *testing.T) {
-	pt,err := Decrypt(ct,msg,msg)
-	if err != nil {
-		t.Fatalf("wanted err == nil, got err = %v", err)
-	}
+		pt, err := Decrypt(ct, msg, msg)
+		if err != nil {
+			t.Fatalf("wanted err == nil, got err = %v", err)
+		}
 
-	wantPT := []byte("Hello World\n")
-	if !bytes.Equal(pt,wantPT) {
-		t.Fatalf("want != got | %x != %x", pt, wantPT)
-	}
+		wantPT := []byte("Hello World\n")
+		if !bytes.Equal(pt, wantPT) {
+			t.Fatalf("want != got | %x != %x", pt, wantPT)
+		}
 	})
 
 	t.Run("bad decryption", func(t *testing.T) {
-		_,err := Decrypt(ct,[]byte("bad key"),msg)
+		_, err := Decrypt(ct, []byte("bad key"), msg)
 		if err == nil {
 			t.Fatalf("wanted err != nil, got err = %v", err)
 		}
@@ -60,16 +59,16 @@ func TestEncryptDecrypt(t *testing.T) {
 	key := []byte("key")
 	IV := []byte("IV")
 
-	ct,err := Encrypt(msg,key,IV)
+	ct, err := Encrypt(msg, key, IV)
 	if err != nil {
 		t.Fatalf("wanted err == nil | got: %v", err)
 	}
-	pt,err := Decrypt(ct,key,IV)
+	pt, err := Decrypt(ct, key, IV)
 	if err != nil {
 		t.Fatalf("wanted err == nil | got: %v", err)
 	}
 
-	if !bytes.Equal(msg,pt) {
+	if !bytes.Equal(msg, pt) {
 		t.Fatalf("want != got | %x != %x", ct, pt)
 	}
 }
